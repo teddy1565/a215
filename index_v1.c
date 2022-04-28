38915a72190025d8543eea8b03754fe2c06d77a1
teddy1565 <teddy1565@gmail.com>
2022-04-29 06:01:05 +0800

    v1

---
+++ index_v1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+    int n,m;
+    while(~scanf("%d %d", &n, &m)) {
+        int sum = 0;
+        int counter = 0;
+        for (int i=n;i<m;i++) {
+            sum+=i;
+            counter++;
+            if (sum > m) {
+                printf("%d\n", counter);
+                break;
+            }
+        }
+        
+    }
+    return 0;
+}
